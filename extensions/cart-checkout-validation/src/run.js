0b1d6cffb846e284b5d14c97b8bf890199a326f4
Digilearning000 <digilearning000@gmail.com>
2024-04-19 17:52:30 -0400

    fixed bug

--- extensions/cart-checkout-validation/src/run.js
+++ extensions/cart-checkout-validation/src/run.js
@@ -10,14 +10,21 @@
  * @returns {FunctionRunResult}
  */
 export function run(input) {
-  const errors = input.cart.lines
-    .filter(({ quantity }) => quantity > 1)
-    .map(() => ({
-      localizedMessage: "Not possible to order more than one of each",
-      target: "cart",
-    }));
+  const errors = [];
+
+  input.cart.lines.forEach((lineItem) => {
+    const { quantity, merchandise } = lineItem;
+
+    const max = Number.parseInt(merchandise?.product?.max_items.value);
+    if (max && quantity > max) {
+      errors.push({
+        message: `Quantity of ${quantity} exceeds max of ${max}`,
+        target: "cart",
+      });
+    }
+  });
 
   return {
-    errors
-  }
-};
+    errors,
+  };
+}
